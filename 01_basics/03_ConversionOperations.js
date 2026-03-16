a890a3d88ba119c57875112c78729074831fe25a
Mridula222 <137818547+Mridula222@users.noreply.github.com>
2026-03-16 17:27:07 +0000

    .

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -1,15 +1,30 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNum = Number(score)
-console.log(typeof valueInNum)
-console.log(valueInNum) //when we checked the type of this it was the number when we are printing the value its coming out to be nan
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum)
+// console.log(valueInNum) //when we checked the type of this it was the number when we are printing the value its coming out to be nan
 
 //"33" => 33
 //"33abc" => NAN
 
-let someNum = 33
+/* let someNum = 33
 let stringNum = String(someNum)
 console.log(stringNum)
-console.log(typeof stringNum)
+console.log(typeof stringNum) */
+
+//------------------------------------------Operations---------------------------------------------
+
+let str1 = "hello"
+let str2 = " Anshika"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 3 + "3")
+
+//=== strictly check checks both value and datatype

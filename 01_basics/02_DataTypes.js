052cc5b39598aab4ec1ba99177724d151aac1eba
Mridula222 <137818547+Mridula222@users.noreply.github.com>
2026-03-16 16:43:12 +0000

    Datatypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all js code as new version
+
+// alert("haalo mr hacker") we are using node.js not browser
+
+console.log("mridula")
+
+//IQ.1- null is an object datatype while undefined has the type as undefined
+
+console.log(typeof null)
+console.log(typeof undefined)

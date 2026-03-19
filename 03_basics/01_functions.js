c03ed479a659cbc0babae8f6a7aa2560b9d41463
Mridula222 <137818547+Mridula222@users.noreply.github.com>
2026-03-19 15:11:31 +0000

    func1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,17 @@
+//reference -> functionName and execution -> functionName()
+
+// function addTwoNum(num1, num2) {//param
+//     console.log(num1 + num2);
+// }
+
+function addTwoNum(num1, num2) {
+    let result = num1 + num2
+    return result
+}
+
+
+// addTwoNum(2, "3")//arg
+
+const result = addTwoNum(3, 4)
+console.log("result: ",result);
+

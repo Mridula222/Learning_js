dc89115026fdf64ccabb4910c5e111350f9c76c7
Mridula222 <137818547+Mridula222@users.noreply.github.com>
2026-03-20 16:51:49 +0000

    funcn-02

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,5 +13,36 @@ function addTwoNum(num1, num2) {
 // addTwoNum(2, "3")//arg
 
 const result = addTwoNum(3, 4)
-console.log("result: ",result);
+// console.log("result: ",result);
 
+
+//++++++++++++++++++++++part-02+++++++++++++++++++++++++++++++++++++++++++
+//if no of parameters are unkown then we can use rest/spread operator
+function calCartPrice(val1,val2,...num1){
+    return num1
+}
+// console.log(calCartPrice(200,300,400))
+
+const user = {
+    userName : "Mridula",
+    price: 200
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.userName} and priceis ${anyObject.price}`);
+}
+
+// handleObject(user)
+
+handleObject({
+    userName: "Anshika",
+    price: 700
+})
+
+const myNewArray = [100, 300, 500, 700]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
